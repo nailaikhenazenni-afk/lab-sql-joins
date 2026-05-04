7541e1b6126541588f2d817d514dcee69fac3b49
nailaikhenazenni-afk <naila.ikhenazen.ni@gmail.com>
2026-05-04 08:15:22 +0200

    Create solved_lab_join.sql

---
+++ solved_lab_join.sql
@@ -0,0 +1,65 @@
+SELECT 
+    c.name AS category_name, 
+    COUNT(fc.film_id) AS total_films
+FROM 
+    category c
+JOIN 
+    film_category fc ON c.category_id = fc.category_id
+GROUP BY 
+    c.name
+ORDER BY 
+    total_films DESC;
+
+SELECT 
+    s.store_id, 
+    ci.city, 
+    co.country
+FROM 
+    store s
+JOIN 
+    address a ON s.address_id = a.address_id
+JOIN 
+    city ci ON a.city_id = ci.city_id
+JOIN 
+    country co ON ci.country_id = co.country_id;
+    
+SELECT 
+    s.store_id, 
+    SUM(p.amount) AS total_revenue_dollars
+FROM 
+    store s
+JOIN 
+    staff st ON s.store_id = st.store_id
+JOIN 
+    payment p ON st.staff_id = p.staff_id
+GROUP BY 
+    s.store_id;
+    
+SELECT 
+    rating, 
+    ROUND(AVG(length), 2) AS average_duration
+FROM 
+    film
+GROUP BY 
+    rating
+ORDER BY 
+    average_duration DESC;
+
+SELECT 
+    s.store_id, 
+    SUM(p.amount) AS total_revenue_dollars
+FROM 
+    store s
+JOIN 
+    staff st ON s.store_id = st.store_id
+JOIN 
+    payment p ON st.staff_id = p.staff_id
+GROUP BY 
+    s.store_id;
+
+**Bonus**:
+
+5.  Identify the film categories with the longest average running time.
+6.  Display the top 10 most frequently rented movies in descending order.
+7. Determine if "Academy Dinosaur" can be rented from Store 1.
+8. Provide a list of all distinct film titles, along with their availability status in the inventory. Include a column indicating whether each title is 'Available' or 'NOT available.' Note that there are 42 titles that are not in the inventory, and this information can be obtained using a `CASE` statement combined with `IFNULL`."
