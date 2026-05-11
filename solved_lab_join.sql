4195988c4692fc9569760ce0bcf46aabf5a6a618
nailaikhenazenni-afk <naila.ikhenazen.ni@gmail.com>
2026-05-11 08:03:21 +0200

    Update solved_lab_join.sql

--- solved_lab_join.sql
+++ solved_lab_join.sql
@@ -1,14 +1,16 @@
 SELECT 
     c.name AS category_name, 
-    COUNT(fc.film_id) AS total_films
+    COUNT(DISTINCT fc.film_id) AS total_films
 FROM 
-    category c
-JOIN 
-    film_category fc ON c.category_id = fc.category_id
+    category AS c
+INNER JOIN 
+    film_category AS fc ON c.category_id = fc.category_id
 GROUP BY 
+    c.category_id, 
     c.name
 ORDER BY 
-    total_films DESC;
+    total_films DESC, 
+    category_name ASC;
 
 SELECT 
     s.store_id, 
@@ -46,24 +48,5 @@ JOIN
     film AS f ON fc.film_id = f.film_id
 GROUP BY 
     c.name
-ORDER BY 
-    average_duration DESC;
+;
 
-SELECT 
-    s.store_id, 
-    SUM(p.amount) AS total_revenue_dollars
-FROM 
-    store s
-JOIN 
-    staff st ON s.store_id = st.store_id
-JOIN 
-    payment p ON st.staff_id = p.staff_id
-GROUP BY 
-    s.store_id;
-
-**Bonus**:
-
-5.  Identify the film categories with the longest average running time.
-6.  Display the top 10 most frequently rented movies in descending order.
-7. Determine if "Academy Dinosaur" can be rented from Store 1.
-8. Provide a list of all distinct film titles, along with their availability status in the inventory. Include a column indicating whether each title is 'Available' or 'NOT available.' Note that there are 42 titles that are not in the inventory, and this information can be obtained using a `CASE` statement combined with `IFNULL`."
